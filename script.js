33c48e4b081316551f1ceaf471b0da14100cda2c
Guilhem SEYVET <guilhemcvdev@outlook.com>
2021-07-31 21:40:38 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,217 @@
+const descri = document.querySelector("#imgun");
+const bout = document.querySelector("#boutonun");
+
+let changement = "Descriptif";
+bout.addEventListener("click", function() {
+    if (changement === "Descriptif"){
+    descri.innerHTML = '<div id="descriptif"><p>Adipisicing consectetur irure id officia ipsum incididunt officia consequat mollit ut sint tempor irure. Laborum dolore cupidatat veniam mollit ad dolor reprehenderit aliqua. Enim sunt labore amet ipsum excepteur eiusmod dolore sunt do pariatur consectetur cillum incididunt minim. Dolor sunt cillum minim consectetur enim anim sit. Consequat non veniam aliqua reprehenderit.</p><h2>Tarif : ...€</h2></div>';
+    descri.removeAttribute("href");
+    bout.innerHTML = "Retour";
+    changement = "Retour";
+    }
+    else {
+        descri.innerHTML ='<a id="imgun" href=https://picsum.photos/id/684/900 target="_blank"><img id="imgun" src="https://picsum.photos/id/250/300" alt=""></a>';
+        bout.innerHTML = "Descriptif";
+        changement = "Descriptif";
+    }
+})
+
+const descriDeux = document.querySelector("#imgdeux");
+const boutDeux = document.querySelector("#boutondeux");
+
+let changementDeux = "Descriptif";
+boutDeux.addEventListener("click", function() {
+    if (changementDeux === "Descriptif"){
+    descriDeux.innerHTML = '<div id="descriptif"><p>Adipisicing consectetur irure id officia ipsum incididunt officia consequat mollit ut sint tempor irure. Laborum dolore cupidatat veniam mollit ad dolor reprehenderit aliqua. Enim sunt labore amet ipsum excepteur eiusmod dolore sunt do pariatur consectetur cillum incididunt minim. Dolor sunt cillum minim consectetur enim anim sit. Consequat non veniam aliqua reprehenderit.</p><h2>Tarif : ...€</h2></div>';
+    descriDeux.removeAttribute("href");
+    boutDeux.innerHTML = "Retour";
+    changementDeux = "Retour";
+    }
+    else {
+        descriDeux.innerHTML ='<a id="imgdeux" href=https://picsum.photos/id/685/900 target="_blank"><img src="https://picsum.photos/id/685/300" alt=""></a>';
+        boutDeux.innerHTML = "Descriptif";
+        changementDeux = "Descriptif";
+    }
+})
+
+const descriTrois = document.querySelector("#imgtrois");
+const boutTrois = document.querySelector("#boutontrois");
+
+let changementTrois = "Descriptif";
+boutTrois.addEventListener("click", function() {
+    if (changementTrois === "Descriptif"){
+    descriTrois.innerHTML = '<div id="descriptif"><p>Adipisicing consectetur irure id officia ipsum incididunt officia consequat mollit ut sint tempor irure. Laborum dolore cupidatat veniam mollit ad dolor reprehenderit aliqua. Enim sunt labore amet ipsum excepteur eiusmod dolore sunt do pariatur consectetur cillum incididunt minim. Dolor sunt cillum minim consectetur enim anim sit. Consequat non veniam aliqua reprehenderit.</p><h2>Tarif : ...€</h2></div>';
+    descriTrois.removeAttribute("href");
+    boutTrois.innerHTML = "Retour";
+    changementTrois = "Retour";
+    }
+    else {
+        descriTrois.innerHTML ='<a id="imgtrois" href=https://picsum.photos/id/686/900 target="_blank"><img src="https://picsum.photos/id/686/300" alt=""></a>';
+        boutTrois.innerHTML = "Descriptif";
+        changementTrois = "Descriptif";
+    }
+})
+
+const descriQuatre = document.querySelector("#imgquatre");
+const boutQuatre = document.querySelector("#boutonquatre");
+
+let changementQuatre = "Descriptif";
+boutQuatre.addEventListener("click", function() {
+    if (changementQuatre === "Descriptif"){
+    descriQuatre.innerHTML = '<div id="descriptif"><p>Adipisicing consectetur irure id officia ipsum incididunt officia consequat mollit ut sint tempor irure. Laborum dolore cupidatat veniam mollit ad dolor reprehenderit aliqua. Enim sunt labore amet ipsum excepteur eiusmod dolore sunt do pariatur consectetur cillum incididunt minim. Dolor sunt cillum minim consectetur enim anim sit. Consequat non veniam aliqua reprehenderit.</p><h2>Tarif : ...€</h2></div>';
+    descriQuatre.removeAttribute("href");
+    boutQuatre.innerHTML = "Retour";
+    changementQuatre = "Retour";
+    }
+    else {
+        descriQuatre.innerHTML ='<a id="imgquatre" href=https://picsum.photos/id/687/900 target="_blank"><img src="https://picsum.photos/id/687/300" alt=""></a>';
+        boutQuatre.innerHTML = "Descriptif";
+        changementQuatre = "Descriptif";
+    }
+})
+
+const descriCinq = document.querySelector("#imgcinq");
+const boutCinq = document.querySelector("#boutoncinq");
+
+let changementCinq = "Descriptif";
+boutCinq.addEventListener("click", function() {
+    if (changementCinq === "Descriptif"){
+    descriCinq.innerHTML = '<div id="descriptif"><p>Adipisicing consectetur irure id officia ipsum incididunt officia consequat mollit ut sint tempor irure. Laborum dolore cupidatat veniam mollit ad dolor reprehenderit aliqua. Enim sunt labore amet ipsum excepteur eiusmod dolore sunt do pariatur consectetur cillum incididunt minim. Dolor sunt cillum minim consectetur enim anim sit. Consequat non veniam aliqua reprehenderit.</p><h2>Tarif : ...€</h2></div>';
+    descriCinq.removeAttribute("href");
+    boutCinq.innerHTML = "Retour";
+    changementCinq = "Retour";
+    }
+    else {
+        descriCinq.innerHTML ='<a id="imgcinq" href=https://picsum.photos/id/688/900 target="_blank"><img src="https://picsum.photos/id/688/300" alt=""></a>';
+        boutCinq.innerHTML = "Descriptif";
+        changementCinq = "Descriptif";
+    }
+})
+
+const descriSix = document.querySelector("#imgsix");
+const boutSix = document.querySelector("#boutonsix");
+
+let changementSix = "Descriptif";
+boutSix.addEventListener("click", function() {
+    if (changementSix === "Descriptif"){
+    descriSix.innerHTML = '<div id="descriptif"><p>Adipisicing consectetur irure id officia ipsum incididunt officia consequat mollit ut sint tempor irure. Laborum dolore cupidatat veniam mollit ad dolor reprehenderit aliqua. Enim sunt labore amet ipsum excepteur eiusmod dolore sunt do pariatur consectetur cillum incididunt minim. Dolor sunt cillum minim consectetur enim anim sit. Consequat non veniam aliqua reprehenderit.</p><h2>Tarif : ...€</h2></div>';
+    descriSix.removeAttribute("href");
+    boutSix.innerHTML = "Retour";
+    changementSix = "Retour";
+    }
+    else {
+        descriSix.innerHTML ='<a id="imgsix" href=https://picsum.photos/id/689/900 target="_blank"><img src="https://picsum.photos/id/689/300" alt=""></a>';
+        boutSix.innerHTML = "Descriptif";
+        changementSix = "Descriptif";
+    }
+})
+
+const descriSept = document.querySelector("#imgsept");
+const boutSept = document.querySelector("#boutonsept");
+
+let changementSept = "Descriptif";
+boutSept.addEventListener("click", function() {
+    if (changementSept === "Descriptif"){
+    descriSept.innerHTML = '<div id="descriptif"><p>Adipisicing consectetur irure id officia ipsum incididunt officia consequat mollit ut sint tempor irure. Laborum dolore cupidatat veniam mollit ad dolor reprehenderit aliqua. Enim sunt labore amet ipsum excepteur eiusmod dolore sunt do pariatur consectetur cillum incididunt minim. Dolor sunt cillum minim consectetur enim anim sit. Consequat non veniam aliqua reprehenderit.</p><h2>Tarif : ...€</h2></div>';
+    descriSept.removeAttribute("href");
+    boutSept.innerHTML = "Retour";
+    changementSept = "Retour";
+    }
+    else {
+        descriSept.innerHTML ='<a id="imgsept" href=https://picsum.photos/id/690/900 target="_blank"><img src="https://picsum.photos/id/690/300" alt=""></a>';
+        boutSept.innerHTML = "Descriptif";
+        changementSept = "Descriptif";
+    }
+})
+
+
+const descriHuit = document.querySelector("#imghuit");
+const boutHuit = document.querySelector("#boutonhuit");
+
+let changementHuit = "Descriptif";
+boutHuit.addEventListener("click", function() {
+    if (changementHuit === "Descriptif"){
+    descriHuit.innerHTML = '<div id="descriptif"><p>Adipisicing consectetur irure id officia ipsum incididunt officia consequat mollit ut sint tempor irure. Laborum dolore cupidatat veniam mollit ad dolor reprehenderit aliqua. Enim sunt labore amet ipsum excepteur eiusmod dolore sunt do pariatur consectetur cillum incididunt minim. Dolor sunt cillum minim consectetur enim anim sit. Consequat non veniam aliqua reprehenderit.</p><h2>Tarif : ...€</h2></div>';
+    descriHuit.removeAttribute("href");
+    boutHuit.innerHTML = "Retour";
+    changementHuit = "Retour";
+    }
+    else {
+        descriHuit.innerHTML ='<a id="imghuit" href=https://picsum.photos/id/691/900 target="_blank"><img src="https://picsum.photos/id/691/300" alt=""></a>';
+        boutHuit.innerHTML = "Descriptif";
+        changementHuit = "Descriptif";
+    }
+})
+
+
+const descriNeuf = document.querySelector("#imgneuf");
+const boutNeuf = document.querySelector("#boutonneuf");
+
+let changementNeuf = "Descriptif";
+boutNeuf.addEventListener("click", function() {
+    if (changementNeuf === "Descriptif"){
+    descriNeuf.innerHTML = '<div id="descriptif"><p>Adipisicing consectetur irure id officia ipsum incididunt officia consequat mollit ut sint tempor irure. Laborum dolore cupidatat veniam mollit ad dolor reprehenderit aliqua. Enim sunt labore amet ipsum excepteur eiusmod dolore sunt do pariatur consectetur cillum incididunt minim. Dolor sunt cillum minim consectetur enim anim sit. Consequat non veniam aliqua reprehenderit.</p><h2>Tarif : ...€</h2></div>';
+    descriNeuf.removeAttribute("href");
+    boutNeuf.innerHTML = "Retour";
+    changementNeuf = "Retour";
+    }
+    else {
+        descriNeuf.innerHTML ='<a id="imgneuf" href=https://picsum.photos/id/692/900 target="_blank"><img src="https://picsum.photos/id/692/300" alt=""></a>';
+        boutNeuf.innerHTML = "Descriptif";
+        changementNeuf = "Descriptif";
+    }
+})
+
+const descriDix = document.querySelector("#imgdix");
+const boutDix = document.querySelector("#boutondix");
+
+let changementDix = "Descriptif";
+boutDix.addEventListener("click", function() {
+    if (changementDix === "Descriptif"){
+    descriDix.innerHTML = '<div id="descriptif"><p>Adipisicing consectetur irure id officia ipsum incididunt officia consequat mollit ut sint tempor irure. Laborum dolore cupidatat veniam mollit ad dolor reprehenderit aliqua. Enim sunt labore amet ipsum excepteur eiusmod dolore sunt do pariatur consectetur cillum incididunt minim. Dolor sunt cillum minim consectetur enim anim sit. Consequat non veniam aliqua reprehenderit.</p><h2>Tarif : ...€</h2></div>';
+    descriDix.removeAttribute("href");
+    boutDix.innerHTML = "Retour";
+    changementDix = "Retour";
+    }
+    else {
+        descriDix.innerHTML ='<a id="imgdix" href=https://picsum.photos/id/693/900 target="_blank"><img src="https://picsum.photos/id/693/300" alt=""></a>';
+        boutDix.innerHTML = "Descriptif";
+        changementDix = "Descriptif";
+    }
+})
+
+const descriOnze = document.querySelector("#imgonze");
+const boutOnze = document.querySelector("#boutononze");
+
+let changementOnze = "Descriptif";
+boutOnze.addEventListener("click", function() {
+    if (changementOnze === "Descriptif"){
+    descriOnze.innerHTML = '<div id="descriptif"><p>Adipisicing consectetur irure id officia ipsum incididunt officia consequat mollit ut sint tempor irure. Laborum dolore cupidatat veniam mollit ad dolor reprehenderit aliqua. Enim sunt labore amet ipsum excepteur eiusmod dolore sunt do pariatur consectetur cillum incididunt minim. Dolor sunt cillum minim consectetur enim anim sit. Consequat non veniam aliqua reprehenderit.</p><h2>Tarif : ...€</h2></div>';
+    descriOnze.removeAttribute("href");
+    boutOnze.innerHTML = "Retour";
+    changementOnze = "Retour";
+    }
+    else {
+        descriOnze.innerHTML ='<a id="imgonze" href=https://picsum.photos/id/694/900 target="_blank"><img src="https://picsum.photos/id/694/300" alt=""></a>';
+        boutOnze.innerHTML = "Descriptif";
+        changementOnze = "Descriptif";
+    }
+})
+
+const descriDouze = document.querySelector("#imgdouze");
+const boutDouze = document.querySelector("#boutondouze");
+
+let changementDouze = "Descriptif";
+boutDouze.addEventListener("click", function() {
+    if (changementDouze === "Descriptif"){
+    descriDouze.innerHTML = '<div id="descriptif"><p>Adipisicing consectetur irure id officia ipsum incididunt officia consequat mollit ut sint tempor irure. Laborum dolore cupidatat veniam mollit ad dolor reprehenderit aliqua. Enim sunt labore amet ipsum excepteur eiusmod dolore sunt do pariatur consectetur cillum incididunt minim. Dolor sunt cillum minim consectetur enim anim sit. Consequat non veniam aliqua reprehenderit.</p><h2>Tarif : ...€</h2></div>';
+    descriDouze.removeAttribute("href");
+    boutDouze.innerHTML = "Retour";
+    changementDouze = "Retour";
+    }
+    else {
+        descriDouze.innerHTML ='<a id="imgdouze" href=https://picsum.photos/id/695/900 target="_blank"><img src="https://picsum.photos/id/695/300" alt=""></a>';
+        boutDouze.innerHTML = "Descriptif";
+        changementDouze = "Descriptif";
+    }
+})
